7e68de16e8d1f7e8951a0fa73e8d3f7547679e05
Ivan Conde <iconde@coelsa.com.ar>
2021-11-01 13:47:30 -0300

    Commit inicial

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities{
+
+    public class AppUser{
+
+        // Propiedad autogenerada / auto implementada.
+        public int Id { get; set; }
+
+        public string UserName { get; set; }
+
+    }
+
+}
