59622fe5b35ac2ec4616540727e64d78ea5f93be
IvanConde <conde.ivanleonardo@gmail.com>
2021-12-14 16:58:27 -0300

    End of Section 13

--- API/Controllers/BaseApiController.cs
+++ API/Controllers/BaseApiController.cs
@@ -1,11 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using API.Helpers;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace API.Controllers {
-
+    [ServiceFilter(typeof(LogUserActivity))]
     [ApiController]
     [Route("api/[Controller]")]
 
